da0e5112f9c65293534d89107f725fe7fd7ce3db
Zach Russell <zach@protechig.com>
2014-10-30 03:42:50 -0400

    fixed layout

--- functions.php
+++ functions.php
@@ -3,8 +3,8 @@
 include_once( get_template_directory() . '/lib/init.php' );
 
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
-define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_NAME', 'WP Temple Sandbox Theme' );
+define( 'CHILD_THEME_URL', 'https://www.wptemple.com/' );
 define( 'CHILD_THEME_VERSION', '2.1.2' );
 
 //* Enqueue Google Fonts
