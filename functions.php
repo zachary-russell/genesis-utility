4ebe81224a6a2a6c7292c5caec26354d6fd67208
Zach Russell <zach@protechig.com>
2014-11-28 18:22:44 -0500

    add normalize as separate include

--- functions.php
+++ functions.php
@@ -39,4 +39,3 @@ remove_action('genesis_after_header', 'genesis_do_nav');
 add_action('genesis_before_header', 'genesis_do_nav');
 
 //* Add viewport meta tag for mobile browsers
-add_theme_support( 'genesis-responsive-viewport' );
