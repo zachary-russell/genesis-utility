95d5c37aba1b5f08d024efff44a3692dafa7d7a9
Zach Russell <zach@protechig.com>
2015-08-19 19:57:21 -0400

    clean up functions

--- functions.php
+++ functions.php
@@ -2,7 +2,6 @@
 //* Start the engine
 include_once( get_template_directory() . '/lib/init.php' );
 
-
 define( 'CHILD_THEME_NAME', 'WP Temple Sandbox Theme' );
 define( 'CHILD_THEME_URL', 'https://www.wptemple.com/' );
 define( 'CHILD_THEME_VERSION', '2.1.2' );
@@ -20,9 +19,6 @@ add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list'
 //* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
 
-//* Add support for custom background
-add_theme_support( 'custom-background' );
-
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
 
@@ -31,10 +27,7 @@ genesis_unregister_layout( 'content-sidebar-sidebar' );
 genesis_unregister_layout( 'sidebar-content-sidebar' );
 genesis_unregister_layout( 'sidebar-sidebar-content' );
 
-//* Move Primary Nav Above Header
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-add_action( 'genesis_before_header', 'genesis_do_nav' );
-
+//* Add support for custom header (Logo)
 add_theme_support( 'custom-header', array(
     'width'             => 300,
     'height'            => 100,
